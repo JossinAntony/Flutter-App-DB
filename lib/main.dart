21b371a22464db830c1098575377cacc695f72e4
JossinAntony <jossinde@gmail.com>
2020-08-23 10:09:57 +0200

    initial commit. login.dart.

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_flutter_db/pages/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LogIn());
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,60 @@
+
+
+import 'package:flutter/material.dart';
+
+class LogIn extends StatelessWidget {
+
+  TextEditingController uNameCntrlr = TextEditingController();
+  TextEditingController uPwdCntrlr = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Log In'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: uNameCntrlr,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  prefixIcon: Icon(Icons.email),
+                  hintText: 'Enter Email',
+                ),
+              ),
+
+              SizedBox(height: 24.0),
+
+              TextField(
+                controller: uPwdCntrlr,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  hintText: 'Enter password',
+                ),
+              ),
+
+              SizedBox(height: 50),
+
+              RaisedButton(
+                onPressed: (){},
+                color: Colors.blue,
+                child: Center(child: Text('Log In')),
+              ),
+            ],
+          )
+        )
+      )
+    );
+  }
+}
